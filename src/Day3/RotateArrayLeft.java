c20229f5a12df577d71c318b4d019465f467f9b2
MohammedAli200 <alimohammed3731@gmail.com>
2026-05-06 23:25:32 +0530

    RotateArrayLeft

---
+++ src/Day3/RotateArrayLeft.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class RotateArrayLeft {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 5};
+
+        int k = 2;
+
+        k = k % arr.length;
+
+        for (int i = 0; i < k; i++) {
+
+            int first = arr[0];
+
+            for (int j = 0; j < arr.length - 1; j++) {
+
+                arr[j] = arr[j + 1];
+
+            }
+
+            arr[arr.length - 1] = first;
+
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+}
