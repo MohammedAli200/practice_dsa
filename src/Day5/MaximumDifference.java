83391454eed6b7672dfe09dc3f5c2577af8c3d61
MohammedAli200 <alimohammed3731@gmail.com>
2026-05-08 23:28:26 +0530

    MaximumDifference

---
+++ src/Day5/MaximumDifference.java
@@ -0,0 +1,17 @@
+package Day5;
+
+public class MaximumDifference {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 10, 6, 4, 8, 1};
+
+        int min = arr[0];
+        int maxDiff = arr[1] - arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            maxDiff = Math.max(maxDiff, arr[i] - min);
+            min = Math.min(min, arr[i]);
+        }
+
+        System.out.println("Maximum Difference: " + maxDiff);
+    }
+}
