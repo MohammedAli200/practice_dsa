2e9b954a4d1b4302220fbfc8340f86ed9b3dcc62
MohammedAli200 <alimohammed3731@gmail.com>
2026-05-09 22:08:36 +0530

    TrappingRainWater

---
+++ src/Day6/TrappingRainWater.java
@@ -0,0 +1,33 @@
+package Day6;
+
+public class TrappingRainWater {
+    public static void main(String[] args) {
+        int[] height = {3, 0, 0, 2, 0, 4};
+
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = 0;
+        int rightMax = 0;
+        int water = 0;
+
+        while (left <= right) {
+            if (height[left] < height[right]) {
+                if (height[left] > leftMax) {
+                    leftMax = height[left];
+                } else {
+                    water += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] > rightMax) {
+                    rightMax = height[right];
+                } else {
+                    water += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+
+        System.out.println("Trapped Water: " + water);
+    }
+}
