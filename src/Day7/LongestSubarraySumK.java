3394775493fa95e6ad03453e0bc8e0cbfdea752b
MohammedAli200 <alimohammed3731@gmail.com>
2026-05-10 22:24:57 +0530

    LongestSubarraySumK

---
+++ src/Day7/LongestSubarraySumK.java
@@ -0,0 +1,34 @@
+package Day7;
+
+import java.util.*;
+
+public class LongestSubarraySumK {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 5, 2, 7, 1, 9};
+        int k = 15;
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int sum = 0;
+        int maxLen = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            sum += arr[i];
+
+            if (sum == k) {
+                maxLen = i + 1;
+            }
+
+            if (map.containsKey(sum - k)) {
+                maxLen = Math.max(maxLen,
+                        i - map.get(sum - k));
+            }
+
+            map.putIfAbsent(sum, i);
+        }
+
+        System.out.println("Longest Length: " + maxLen);
+    }
+}
