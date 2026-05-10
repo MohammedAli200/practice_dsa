1239b3452640e8d1ba720674a547b5d1991cd6d9
MohammedAli200 <alimohammed3731@gmail.com>
2026-05-10 22:26:30 +0530

    NextGreaterElement

---
+++ src/Day7/NextGreaterElement.java
@@ -0,0 +1,30 @@
+package Day7;
+
+import java.util.*;
+
+public class NextGreaterElement {
+    public static void main(String[] args) {
+
+        int[] arr = {4, 5, 2, 25};
+
+        Stack<Integer> stack = new Stack<>();
+
+        int[] result = new int[arr.length];
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+
+            while (!stack.isEmpty()
+                    && stack.peek() <= arr[i]) {
+                stack.pop();
+            }
+
+            result[i] = stack.isEmpty()
+                    ? -1
+                    : stack.peek();
+
+            stack.push(arr[i]);
+        }
+
+        System.out.println(Arrays.toString(result));
+    }
+}
