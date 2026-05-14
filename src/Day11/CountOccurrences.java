2dff02613b38e8693786e99950f990df98f5e1e5
MohammedAli200 <alimohammed3731@gmail.com>
2026-05-14 22:41:30 +0530

    CountOccurrences

---
+++ src/Day11/CountOccurrences.java
@@ -0,0 +1,21 @@
+package Day11;
+
+public class CountOccurrences {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 2, 2, 3, 4};
+        int target = 2;
+
+        int count = 0;
+
+        for (int num : arr) {
+
+            if (num == target) {
+                count++;
+            }
+        }
+
+        System.out.println("Occurrences: "
+                + count);
+    }
+}
