559f4bd51aa5eb6fb138e130866472b7c275d779
MohammedAli200 <alimohammed3731@gmail.com>
2026-05-14 22:45:05 +0530

    MaximumAverageSubarray

---
+++ src/Day11/MaximumAverageSubarray.java
@@ -0,0 +1,29 @@
+package Day11;
+
+public class MaximumAverageSubarray {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 12, -5, -6, 50, 3};
+        int k = 4;
+
+        int sum = 0;
+
+        for (int i = 0; i < k; i++) {
+            sum += arr[i];
+        }
+
+        int maxSum = sum;
+
+        for (int i = k; i < arr.length; i++) {
+
+            sum += arr[i] - arr[i - k];
+
+            maxSum = Math.max(maxSum, sum);
+        }
+
+        double average = (double) maxSum / k;
+
+        System.out.println("Maximum Average: "
+                + average);
+    }
+}
