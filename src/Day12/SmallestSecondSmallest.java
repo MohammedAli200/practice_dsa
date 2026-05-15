f98087749954919291de50b6be3cc8479ed35c6e
MohammedAli200 <alimohammed3731@gmail.com>
2026-05-15 20:58:16 +0530

    SmallestSecondSmallest

---
+++ src/Day12/SmallestSecondSmallest.java
@@ -0,0 +1,32 @@
+package Day12;
+
+public class SmallestSecondSmallest {
+    public static void main(String[] args) {
+
+        int[] arr = {12, 13, 1, 10, 34, 1};
+
+        int smallest = Integer.MAX_VALUE;
+        int secondSmallest = Integer.MAX_VALUE;
+
+        for (int num : arr) {
+
+            if (num < smallest) {
+
+                secondSmallest = smallest;
+                smallest = num;
+            }
+
+            else if (num < secondSmallest
+                    && num != smallest) {
+
+                secondSmallest = num;
+            }
+        }
+
+        System.out.println("Smallest: "
+                + smallest);
+
+        System.out.println("Second Smallest: "
+                + secondSmallest);
+    }
+}
