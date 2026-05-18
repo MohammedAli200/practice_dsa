bff187b718e5eeb16770f7a1403d3f2d66bebc4d
MohammedAli200 <alimohammed3731@gmail.com>
2026-05-18 20:58:56 +0530

    LongestDecreasingSubsequence

---
+++ src/Day15/LongestDecreasingSubsequence.java
@@ -0,0 +1,32 @@
+package Day15;
+
+public class LongestDecreasingSubsequence {
+    public static void main(String[] args) {
+
+        int[] arr = {9, 4, 3, 2, 5, 4, 3, 2};
+
+        int[] dp = new int[arr.length];
+
+        int maxLength = 1;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            dp[i] = 1;
+
+            for (int j = 0; j < i; j++) {
+
+                if (arr[i] < arr[j]) {
+
+                    dp[i] = Math.max(dp[i],
+                            dp[j] + 1);
+                }
+            }
+
+            maxLength = Math.max(maxLength,
+                    dp[i]);
+        }
+
+        System.out.println("LDS Length: "
+                + maxLength);
+    }
+}
