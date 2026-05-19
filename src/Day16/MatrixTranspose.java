21a08e105b77a3753b73659dfecb1c7507a375a1
MohammedAli200 <alimohammed3731@gmail.com>
2026-05-19 22:29:55 +0530

    MatrixTranspose

---
+++ src/Day16/MatrixTranspose.java
@@ -0,0 +1,43 @@
+package Day16;
+
+public class MatrixTranspose {
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6}
+        };
+
+        int rows = matrix.length;
+        int cols =
+                matrix[0].length;
+
+        int[][] transpose =
+                new int[cols][rows];
+
+        for (int i = 0;
+             i < rows;
+             i++) {
+
+            for (int j = 0;
+                 j < cols;
+                 j++) {
+
+                transpose[j][i] =
+                        matrix[i][j];
+            }
+        }
+
+        for (int[] row : transpose) {
+
+            for (int num : row) {
+
+                System.out.print(
+                        num + " "
+                );
+            }
+
+            System.out.println();
+        }
+    }
+}
