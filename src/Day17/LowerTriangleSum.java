ea8dde23de8df00938ccc548650639c12794d0ab
MohammedAli200 <alimohammed3731@gmail.com>
2026-05-20 22:30:24 +0530

    LowerTriangleSum

---
+++ src/Day17/LowerTriangleSum.java
@@ -0,0 +1,31 @@
+package Day17;
+
+public class LowerTriangleSum {
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        int sum = 0;
+
+        for (int i = 0;
+             i < matrix.length;
+             i++) {
+
+            for (int j = 0;
+                 j <= i;
+                 j++) {
+
+                sum += matrix[i][j];
+            }
+        }
+
+        System.out.println(
+                "Lower Triangle Sum: "
+                        + sum
+        );
+    }
+}
