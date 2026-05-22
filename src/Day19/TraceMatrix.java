a920d2b88895b244a9b42a9ff9c32295c8300f92
MohammedAli200 <alimohammed3731@gmail.com>
2026-05-22 22:50:35 +0530

    TraceMatrix

--- src/Day19/TraceMatrix.java
+++ src/Day19/TraceMatrix.java
@@ -1,4 +1,26 @@
 package Day19;
 
 public class TraceMatrix {
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+                {2, 4, 1},
+                {5, 3, 7},
+                {8, 6, 9}
+        };
+
+        int trace = 0;
+
+        for (int i = 0;
+             i < matrix.length;
+             i++) {
+
+            trace += matrix[i][i];
+        }
+
+        System.out.println(
+                "Trace: "
+                        + trace
+        );
+    }
 }
