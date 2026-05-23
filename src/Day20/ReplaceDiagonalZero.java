eb311fe6f1d51a99d5a97e77dfdb2b12922dfe59
MohammedAli200 <alimohammed3731@gmail.com>
2026-05-23 22:29:44 +0530

    ReplaceDiagonalZero

---
+++ src/Day20/ReplaceDiagonalZero.java
@@ -0,0 +1,31 @@
+package Day20;
+
+public class ReplaceDiagonalZero {
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        for (int i = 0;
+             i < matrix.length;
+             i++) {
+
+            matrix[i][i] = 0;
+        }
+
+        for (int[] row : matrix) {
+
+            for (int num : row) {
+
+                System.out.print(
+                        num + " "
+                );
+            }
+
+            System.out.println();
+        }
+    }
+}
