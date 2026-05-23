9436d1543081bcbeebdbe48ce27c32c256398770
MohammedAli200 <alimohammed3731@gmail.com>
2026-05-23 22:31:45 +0530

    BoundarySumMatrix

---
+++ src/Day20/BoundarySumMatrix.java
@@ -0,0 +1,41 @@
+package Day20;
+
+public class BoundarySumMatrix {
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        int sum = 0;
+
+        int rows = matrix.length;
+        int cols =
+                matrix[0].length;
+
+        for (int i = 0;
+             i < rows;
+             i++) {
+
+            for (int j = 0;
+                 j < cols;
+                 j++) {
+
+                if (i == 0
+                        || j == 0
+                        || i == rows - 1
+                        || j == cols - 1) {
+
+                    sum += matrix[i][j];
+                }
+            }
+        }
+
+        System.out.println(
+                "Boundary Sum: "
+                        + sum
+        );
+    }
+}
