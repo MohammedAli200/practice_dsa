ee0a65ac5993e07a05308bab9a9022982f6b8cd7
MohammedAli200 <alimohammed3731@gmail.com>
2026-05-24 22:46:12 +0530

    ReplaceNegativeZero

---
+++ src/Day21/ReplaceNegativeZero.java
@@ -0,0 +1,39 @@
+package Day21;
+
+public class ReplaceNegativeZero {
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+                {1, -2, 3},
+                {-4, 5, -6},
+                {7, -8, 9}
+        };
+
+        for (int i = 0;
+             i < matrix.length;
+             i++) {
+
+            for (int j = 0;
+                 j < matrix[i].length;
+                 j++) {
+
+                if (matrix[i][j] < 0) {
+
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        for (int[] row : matrix) {
+
+            for (int num : row) {
+
+                System.out.print(
+                        num + " "
+                );
+            }
+
+            System.out.println();
+        }
+    }
+}
