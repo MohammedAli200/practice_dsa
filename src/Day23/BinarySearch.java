3c6c07eda0eee7b4fadc678678577c29fd7d7931
MohammedAli200 <alimohammed3731@gmail.com>
2026-05-26 22:47:21 +0530

    BinarySearch

---
+++ src/Day23/BinarySearch.java
@@ -0,0 +1,44 @@
+package Day23;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+
+        int[] arr = {
+                2, 4, 6, 8, 10, 12
+        };
+
+        int target = 8;
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+
+            int mid =
+                    left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+
+                System.out.println(
+                        "Element Found at Index: "
+                                + mid
+                );
+
+                return;
+            }
+
+            if (arr[mid] < target) {
+
+                left = mid + 1;
+
+            } else {
+
+                right = mid - 1;
+            }
+        }
+
+        System.out.println(
+                "Element Not Found"
+        );
+    }
+}
