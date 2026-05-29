6a79bdd00337c4f074257f733ec7c1cef920c582
MohammedAli200 <alimohammed3731@gmail.com>
2026-05-29 23:57:58 +0530

    RotateRightK

---
+++ src/day26/RotateRightK.java
@@ -0,0 +1,39 @@
+package day26;
+
+import java.util.*;
+
+public class RotateRightK {
+    public static void main(String[] args) {
+
+        int[] arr = {
+                1, 2, 3, 4, 5, 6
+        };
+
+        int k = 2;
+
+        k = k % arr.length;
+
+        reverse(arr, 0, arr.length - 1);
+        reverse(arr, 0, k - 1);
+        reverse(arr, k, arr.length - 1);
+
+        System.out.println(
+                Arrays.toString(arr)
+        );
+    }
+
+    static void reverse(int[] arr,
+                        int start,
+                        int end) {
+
+        while (start < end) {
+
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+}
