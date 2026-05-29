5b5ad27835f6bc9497a2ca71a3fd26a4a74e9161
MohammedAli200 <alimohammed3731@gmail.com>
2026-05-29 23:59:06 +0530

    ArrayPalindrom

---
+++ src/day26/ArrayPalindrome.java
@@ -0,0 +1,34 @@
+package day26;
+
+public class ArrayPalindrome {
+    public static void main(String[] args) {
+
+        int[] arr = {
+                1, 2, 3, 2, 1
+        };
+
+        boolean palindrome = true;
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+
+            if (arr[left]
+                    != arr[right]) {
+
+                palindrome = false;
+                break;
+            }
+
+            left++;
+            right--;
+        }
+
+        System.out.println(
+                palindrome
+                        ? "Palindrome Array"
+                        : "Not Palindrome"
+        );
+    }
+}
