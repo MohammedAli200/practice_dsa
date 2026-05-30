9b4c283a3e40ecd962f9e23056bb1f92401214e5
MohammedAli200 <alimohammed3731@gmail.com>
2026-05-30 23:23:34 +0530

    SmallestMissingPositive

---
+++ src/day27/SmallestMissingPositive.java
@@ -0,0 +1,33 @@
+package day27;
+
+import java.util.*;
+
+public class SmallestMissingPositive {
+    public static void main(String[] args) {
+
+        int[] arr = {
+                3, 4, -1, 1
+        };
+
+        Set<Integer> set =
+                new HashSet<>();
+
+        for (int num : arr) {
+
+            if (num > 0) {
+                set.add(num);
+            }
+        }
+
+        int missing = 1;
+
+        while (set.contains(missing)) {
+            missing++;
+        }
+
+        System.out.println(
+                "Smallest Missing Positive: "
+                        + missing
+        );
+    }
+}
