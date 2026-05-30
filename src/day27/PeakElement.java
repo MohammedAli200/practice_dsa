712742a0bf8158fa44027a59b0ba949e271aa510
MohammedAli200 <alimohammed3731@gmail.com>
2026-05-30 23:25:00 +0530

    PeakElement

---
+++ src/day27/PeakElement.java
@@ -0,0 +1,33 @@
+package day27;
+
+public class PeakElement {
+    public static void main(String[] args) {
+
+        int[] arr = {
+                1, 3, 20, 4, 1, 0
+        };
+
+        for (int i = 0;
+             i < arr.length;
+             i++) {
+
+            boolean left =
+                    (i == 0)
+                            || (arr[i] >= arr[i - 1]);
+
+            boolean right =
+                    (i == arr.length - 1)
+                            || (arr[i] >= arr[i + 1]);
+
+            if (left && right) {
+
+                System.out.println(
+                        "Peak Element: "
+                                + arr[i]
+                );
+
+                break;
+            }
+        }
+    }
+}
