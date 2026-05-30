4cfddb28af6d91dc96bcd48fb83d5caa969d8126
MohammedAli200 <alimohammed3731@gmail.com>
2026-05-30 23:13:29 +0530

    MajorityElement

---
+++ src/day26/MajorityElement.java
@@ -0,0 +1,36 @@
+package day26;
+
+import java.util.*;
+
+public class MajorityElement {
+    public static void main(String[] args) {
+
+        int[] arr = {2, 2, 1, 1, 2, 2, 2};
+
+        int candidate = 0;
+        int count = 0;
+
+        for (int num : arr) {
+
+            if (count == 0) {
+                candidate = num;
+            }
+
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        count = 0;
+
+        for (int num : arr) {
+            if (num == candidate) {
+                count++;
+            }
+        }
+
+        if (count > arr.length / 2) {
+            System.out.println("Majority Element: " + candidate);
+        } else {
+            System.out.println("No Majority Element");
+        }
+    }
+}
