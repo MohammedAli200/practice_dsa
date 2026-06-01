f24c733914887d8c125f5801ec1f5cb442d1b1ce
MohammedAli200 <alimohammed3731@gmail.com>
2026-06-01 22:41:15 +0530

    MaxSumNonAdjacent

---
+++ src/Day29/MaxSumNonAdjacent.java
@@ -0,0 +1,26 @@
+package Day29;
+
+public class MaxSumNonAdjacent {
+
+    public static void main(String[] args) {
+
+        int[] arr = {5, 5, 10, 100, 10, 5};
+
+        int include = arr[0];
+        int exclude = 0;
+
+        for (int i = 1; i < arr.length; i++) {
+
+            int newExclude =
+                    Math.max(include, exclude);
+
+            include = exclude + arr[i];
+            exclude = newExclude;
+        }
+
+        System.out.println(
+                "Maximum Sum = "
+                        + Math.max(include, exclude)
+        );
+    }
+}
