e55aedcf85fdc1ec659a7e491c4dde1009e21841
MohammedAli200 <alimohammed3731@gmail.com>
2026-06-02 22:53:29 +0530

    MoveZerosToEnd

---
+++ src/Day30/MoveZerosToEnd.java
@@ -0,0 +1,28 @@
+package Day30;
+
+import java.util.*;
+
+public class MoveZerosToEnd {
+
+    public static void main(String[] args) {
+
+        int[] arr = {0,1,0,3,12};
+
+        int index = 0;
+
+        for (int num : arr) {
+
+            if (num != 0) {
+                arr[index++] = num;
+            }
+        }
+
+        while (index < arr.length) {
+            arr[index++] = 0;
+        }
+
+        System.out.println(
+                Arrays.toString(arr)
+        );
+    }
+}
