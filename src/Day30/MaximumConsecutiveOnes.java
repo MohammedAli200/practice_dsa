cdefe698e83c0ba4e01f76c9eb5318f32c783fe2
MohammedAli200 <alimohammed3731@gmail.com>
2026-06-02 22:40:18 +0530

    MaximumConsecutiveOnes

---
+++ src/Day30/MaximumConsecutiveOnes.java
@@ -0,0 +1,27 @@
+package Day30;
+
+public class MaximumConsecutiveOnes {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1,1,0,1,1,1};
+
+        int max = 0;
+        int count = 0;
+
+        for (int num : arr) {
+
+            if (num == 1) {
+
+                count++;
+                max = Math.max(max, count);
+
+            } else {
+
+                count = 0;
+            }
+        }
+
+        System.out.println(max);
+    }
+}
