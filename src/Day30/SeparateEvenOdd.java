09f5a581e6c8e6bd40afb148ed0f22f4fcb07820
MohammedAli200 <alimohammed3731@gmail.com>
2026-06-02 23:17:28 +0530

    SeparateEvenOdd

---
+++ src/Day30/SeparateEvenOdd.java
@@ -0,0 +1,36 @@
+package Day30;
+
+import java.util.*;
+
+public class SeparateEvenOdd {
+
+    public static void main(String[] args) {
+
+        int[] arr = {12,17,70,15,22,65,21,90};
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+
+            while (arr[left] % 2 == 0) {
+                left++;
+            }
+
+            while (arr[right] % 2 != 0) {
+                right--;
+            }
+
+            if (left < right) {
+
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+            }
+        }
+
+        System.out.println(
+                Arrays.toString(arr)
+        );
+    }
+}
