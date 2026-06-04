2f82108ca3496d54ba1f0e9ee211d9db62a99376
MohammedAli200 <alimohammed3731@gmail.com>
2026-06-04 23:50:26 +0530

    InsertInterval

---
+++ src/Day31/InsertInterval.java
@@ -0,0 +1,57 @@
+package Day31;
+
+import java.util.*;
+
+public class InsertInterval {
+
+    public static void main(String[] args) {
+
+        int[][] intervals = {
+                {1,3},
+                {6,9}
+        };
+
+        int[] newInterval = {2,5};
+
+        List<int[]> result =
+                new ArrayList<>();
+
+        int i = 0;
+
+        while (i < intervals.length &&
+                intervals[i][1] < newInterval[0]) {
+
+            result.add(intervals[i++]);
+        }
+
+        while (i < intervals.length &&
+                intervals[i][0] <= newInterval[1]) {
+
+            newInterval[0] =
+                    Math.min(newInterval[0],
+                            intervals[i][0]);
+
+            newInterval[1] =
+                    Math.max(newInterval[1],
+                            intervals[i][1]);
+
+            i++;
+        }
+
+        result.add(newInterval);
+
+        while (i < intervals.length) {
+
+            result.add(intervals[i++]);
+        }
+
+        for (int[] interval : result) {
+
+            System.out.println(
+                    interval[0]
+                            + " "
+                            + interval[1]
+            );
+        }
+    }
+}
