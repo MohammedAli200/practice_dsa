80dd6080fa84455497668a0f7b508649cd268994
MohammedAli200 <alimohammed3731@gmail.com>
2026-06-05 22:57:01 +0530

    MaximumMeetings

---
+++ src/Day32/MaximumMeetings.java
@@ -0,0 +1,54 @@
+package Day32;
+
+import java.util.*;
+
+public class MaximumMeetings {
+
+    public static void main(String[] args) {
+
+        int[] start = {
+                1, 3, 0, 5,
+                8, 5
+        };
+
+        int[] end = {
+                2, 4, 6, 7,
+                9, 9
+        };
+
+        int n = start.length;
+
+        int[][] meetings =
+                new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+
+            meetings[i][0] = start[i];
+            meetings[i][1] = end[i];
+        }
+
+        Arrays.sort(
+                meetings,
+                Comparator.comparingInt(a -> a[1])
+        );
+
+        int count = 1;
+        int lastEnd =
+                meetings[0][1];
+
+        for (int i = 1; i < n; i++) {
+
+            if (meetings[i][0]
+                    > lastEnd) {
+
+                count++;
+                lastEnd =
+                        meetings[i][1];
+            }
+        }
+
+        System.out.println(
+                "Meetings = " + count
+        );
+    }
+}
