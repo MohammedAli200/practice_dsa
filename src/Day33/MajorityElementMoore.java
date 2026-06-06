2dbe93b68646c222f5e46520d68c650a23a189cf
MohammedAli200 <alimohammed3731@gmail.com>
2026-06-06 21:06:41 +0530

    MajorityElementMoore

---
+++ src/Day33/MajorityElementMoore.java
@@ -0,0 +1,26 @@
+package Day33;
+
+public class MajorityElementMoore {
+
+    public static void main(String[] args) {
+
+        int[] arr = {2,2,1,1,1,2,2};
+
+        int candidate = 0;
+        int count = 0;
+
+        for (int num : arr) {
+
+            if (count == 0) {
+                candidate = num;
+            }
+
+            count +=
+                    (num == candidate)
+                            ? 1
+                            : -1;
+        }
+
+        System.out.println(candidate);
+    }
+}
