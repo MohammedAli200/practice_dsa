3ae5c0c71b08533f5abd6290e406656bdb1149c8
MohammedAli200 <alimohammed3731@gmail.com>
2026-06-09 22:39:36 +0530

    StockSpanProblem

---
+++ src/Day36/StockSpanProblem.java
@@ -0,0 +1,43 @@
+package Day36;
+
+import java.util.*;
+
+public class StockSpanProblem {
+
+    public static void main(String[] args) {
+
+        int[] prices = {
+                100, 80, 60,
+                70, 60, 75, 85
+        };
+
+        int[] span =
+                new int[prices.length];
+
+        Stack<Integer> stack =
+                new Stack<>();
+
+        for (int i = 0;
+             i < prices.length;
+             i++) {
+
+            while (!stack.isEmpty()
+                    && prices[stack.peek()]
+                    <= prices[i]) {
+
+                stack.pop();
+            }
+
+            span[i] =
+                    stack.isEmpty()
+                            ? i + 1
+                            : i - stack.peek();
+
+            stack.push(i);
+        }
+
+        System.out.println(
+                Arrays.toString(span)
+        );
+    }
+}
