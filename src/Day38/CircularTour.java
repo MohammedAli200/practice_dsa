5e33205b41400d98c43fd99d71c4ffb5c65fe61f
MohammedAli200 <alimohammed3731@gmail.com>
2026-06-11 21:21:16 +0530

    CircularTour

---
+++ src/Day38/CircularTour.java
@@ -0,0 +1,32 @@
+package Day38;
+
+public class CircularTour {
+
+    public static void main(String[] args) {
+
+        int[] petrol = {4, 6, 7, 4};
+        int[] distance = {6, 5, 3, 5};
+
+        int start = 0;
+        int balance = 0;
+        int deficit = 0;
+
+        for (int i = 0; i < petrol.length; i++) {
+
+            balance += petrol[i] - distance[i];
+
+            if (balance < 0) {
+
+                start = i + 1;
+                deficit += balance;
+                balance = 0;
+            }
+        }
+
+        if (balance + deficit >= 0) {
+            System.out.println(start);
+        } else {
+            System.out.println(-1);
+        }
+    }
+}
