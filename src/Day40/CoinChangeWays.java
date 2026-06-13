8303c54ce430aa43b5ad69b9777aeb70f04fe25c
MohammedAli200 <alimohammed3731@gmail.com>
2026-06-13 15:44:46 +0530

    CoinChangeWays

---
+++ src/Day40/CoinChangeWays.java
@@ -0,0 +1,27 @@
+package Day40;
+
+public class CoinChangeWays {
+
+    public static void main(String[] args) {
+
+        int[] coins = {1, 2, 5};
+
+        int amount = 5;
+
+        int[] dp = new int[amount + 1];
+
+        dp[0] = 1;
+
+        for (int coin : coins) {
+
+            for (int i = coin;
+                 i <= amount;
+                 i++) {
+
+                dp[i] += dp[i - coin];
+            }
+        }
+
+        System.out.println(dp[amount]);
+    }
+}
