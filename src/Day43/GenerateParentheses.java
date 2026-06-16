89ba8e78ac10dfc34f1c70c1d01bb10bf5370f3d
MohammedAli200 <alimohammed3731@gmail.com>
2026-06-16 22:16:54 +0530

    GenerateParentheses

---
+++ src/Day43/GenerateParentheses.java
@@ -0,0 +1,46 @@
+package Day43;
+
+import java.util.*;
+
+public class GenerateParentheses {
+
+    static void generate(
+            String current,
+            int open,
+            int close,
+            int n) {
+
+        if (current.length() == 2 * n) {
+
+            System.out.println(current);
+            return;
+        }
+
+        if (open < n) {
+
+            generate(
+                    current + "(",
+                    open + 1,
+                    close,
+                    n
+            );
+        }
+
+        if (close < open) {
+
+            generate(
+                    current + ")",
+                    open,
+                    close + 1,
+                    n
+            );
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int n = 3;
+
+        generate("", 0, 0, n);
+    }
+}
