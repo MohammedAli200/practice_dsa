504098bd8fca81596e12194163f9ede4cdc64cc0
MohammedAli200 <alimohammed3731@gmail.com>
2026-06-18 23:28:39 +0530

    CountPairs

--- src/Day45/CountPairs.java
+++ src/Day45/CountPairs.java
@@ -2,12 +2,13 @@ package Day45;
 
 import java.util.Scanner;
 
-public class ClassicTwoSum {
+public class CountPairs {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the array");
         int size = sc.nextInt();
         int arr[] = new int[size];
+        int count =0;
         for (int i=0;i<arr.length;i++){
             arr[i]=sc.nextInt();
         }
@@ -16,9 +17,10 @@ public class ClassicTwoSum {
         for(int i=0;i<arr.length;i++){
             for (int j=i+1;j<arr.length;j++){
                 if(arr[i]+arr[j]==target){
-                    System.out.println("indexes are "+i+","+j);
+                   count=count+1;
                 }
             }
         }
+        System.out.println(count);
     }
 }
