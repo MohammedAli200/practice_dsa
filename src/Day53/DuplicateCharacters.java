25c5d9726dec33bb98fdbaad22200be6aa7458f1
MohammedAli200 <alimohammed3731@gmail.com>
2026-06-26 23:35:24 +0530

    DuplicateCharacters

---
+++ src/Day53/DuplicateCharacters.java
@@ -0,0 +1,33 @@
+package Day53;
+
+import java.util.Scanner;
+
+public class DuplicateCharacters {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a string:");
+        String str = sc.nextLine();
+
+        boolean[] visited = new boolean[str.length()];
+
+        for (int i = 0; i < str.length(); i++) {
+            if (visited[i]) {
+                continue;
+            }
+
+            int count = 1;
+
+            for (int j = i + 1; j < str.length(); j++) {
+                if (str.charAt(i) == str.charAt(j)) {
+                    count++;
+                    visited[j] = true;
+                }
+            }
+
+            if (count > 1) {
+                System.out.println(str.charAt(i));
+            }
+        }
+    }
+}
