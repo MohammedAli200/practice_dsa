6e55d6600eb4b1b9fe4ec1f1c69a0047a22e340c
MohammedAli200 <alimohammed3731@gmail.com>
2026-06-27 23:35:48 +0530

    ReverseEachWord

---
+++ src/Day54/ReverseEachWord.java
@@ -0,0 +1,21 @@
+package Day54;
+
+import java.util.Scanner;
+
+public class ReverseEachWord {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a sentence:");
+        String str = sc.nextLine();
+
+        String[] words = str.split(" ");
+
+        for (String word : words) {
+            for (int i = word.length() - 1; i >= 0; i--) {
+                System.out.print(word.charAt(i));
+            }
+            System.out.print(" ");
+        }
+    }
+}
