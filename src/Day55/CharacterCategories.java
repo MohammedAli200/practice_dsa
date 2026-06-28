16d9569c146bbdec9dbe07ba5be70937f538146f
MohammedAli200 <alimohammed3731@gmail.com>
2026-06-28 22:56:59 +0530

    CharacterCategories

---
+++ src/Day55/CharacterCategories.java
@@ -0,0 +1,36 @@
+package Day55;
+
+import java.util.Scanner;
+
+public class CharacterCategories {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a string:");
+        String str = sc.nextLine();
+
+        int upper = 0;
+        int lower = 0;
+        int digits = 0;
+        int special = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (Character.isUpperCase(ch)) {
+                upper++;
+            } else if (Character.isLowerCase(ch)) {
+                lower++;
+            } else if (Character.isDigit(ch)) {
+                digits++;
+            } else {
+                special++;
+            }
+        }
+
+        System.out.println("Uppercase = " + upper);
+        System.out.println("Lowercase = " + lower);
+        System.out.println("Digits = " + digits);
+        System.out.println("Special Characters = " + special);
+    }
+}
