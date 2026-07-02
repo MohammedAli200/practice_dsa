81e5e275c6be7925ae2563c8ea76d47458a95ce5
MohammedAli200 <alimohammed3731@gmail.com>
2026-07-02 23:29:15 +0530

    ASCIIValues

---
+++ src/Day59/ASCIIValues.java
@@ -0,0 +1,18 @@
+package Day59;
+
+import java.util.Scanner;
+
+public class ASCIIValues {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a string:");
+        String str = sc.nextLine();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            System.out.println(ch + " -> " + (int) ch);
+        }
+    }
+}
