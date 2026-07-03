0c2e481727b8eaf36101136c923fd14174085b9e
MohammedAli200 <alimohammed3731@gmail.com>
2026-07-03 23:15:26 +0530

    DistinctCharacters

---
+++ src/Day60/DistinctCharacters.java
@@ -0,0 +1,22 @@
+package Day60;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class DistinctCharacters {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a string:");
+        String str = sc.nextLine();
+
+        HashSet<Character> set = new HashSet<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            set.add(str.charAt(i));
+        }
+
+        System.out.println("Distinct Characters: "
+                + set.size());
+    }
+}
