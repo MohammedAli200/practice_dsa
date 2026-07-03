5ec667ab1c9611ec57d5d929496c3cb6e69bbd77
MohammedAli200 <alimohammed3731@gmail.com>
2026-07-03 23:12:49 +0530

    RemoveConsecutiveDuplicates

---
+++ src/Day60/RemoveConsecutiveDuplicates.java
@@ -0,0 +1,28 @@
+package Day60;
+
+import java.util.Scanner;
+
+public class RemoveConsecutiveDuplicates {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a string:");
+        String str = sc.nextLine();
+
+        if (str.length() == 0) {
+            System.out.println("Empty String");
+            return;
+        }
+
+        String result = "";
+        result += str.charAt(0);
+
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) != str.charAt(i - 1)) {
+                result += str.charAt(i);
+            }
+        }
+
+        System.out.println("Result: " + result);
+    }
+}
