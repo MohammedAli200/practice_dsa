9bae77686be162ed63dd3d61489d0291c266d91b
MohammedAli200 <alimohammed3731@gmail.com>
2026-07-08 23:34:52 +0530

    Suffixes

---
+++ src/Day65/Suffixes.java
@@ -0,0 +1,17 @@
+package Day65;
+
+import java.util.Scanner;
+
+public class Suffixes {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println(str.substring(i));
+        }
+    }
+}
