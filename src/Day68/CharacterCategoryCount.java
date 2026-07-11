8e07419e20db7bfb6b508a2186f150e1f0aae90b
MohammedAli200 <alimohammed3731@gmail.com>
2026-07-11 23:18:51 +0530

    CharacterCategoryCount

---
+++ src/Day68/CharacterCategoryCount.java
@@ -0,0 +1,38 @@
+package Day68;
+
+import java.util.Scanner;
+
+public class CharacterCategoryCount {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        int upper = 0, lower = 0, digit = 0, special = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+
+            if (Character.isUpperCase(ch))
+                upper++;
+
+            else if (Character.isLowerCase(ch))
+                lower++;
+
+            else if (Character.isDigit(ch))
+                digit++;
+
+            else
+                special++;
+        }
+
+        System.out.println("Uppercase : " + upper);
+        System.out.println("Lowercase : " + lower);
+        System.out.println("Digits : " + digit);
+        System.out.println("Special Characters : " + special);
+    }
+}
