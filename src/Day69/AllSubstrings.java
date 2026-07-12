1b0f7a7759b5287d7cb078fc7b368f3693abe9ac
MohammedAli200 <alimohammed3731@gmail.com>
2026-07-12 22:58:54 +0530

    AllSubstrings

---
+++ src/Day69/AllSubstrings.java
@@ -0,0 +1,22 @@
+package Day69;
+
+import java.util.Scanner;
+
+public class AllSubstrings {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        for (int i = 0; i < str.length(); i++) {
+
+            for (int j = i + 1; j <= str.length(); j++) {
+
+                System.out.println(str.substring(i, j));
+            }
+        }
+    }
+}
