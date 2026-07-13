94d6b20f5eddf3032c83af83914ec1a0f3e4600a
MohammedAli200 <alimohammed3731@gmail.com>
2026-07-13 23:13:49 +0530

    Palindrome

---
+++ src/Day70/Palindrome.java
@@ -0,0 +1,18 @@
+package Day70;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num=sc.nextInt();
+        int palindrome=0;
+        while(num>0){
+            int rem=num%10;
+            palindrome=rem+palindrome*10;
+            System.out.println(palindrome);
+            num/=10;
+        }
+        System.out.println(palindrome);
+    }
+}
