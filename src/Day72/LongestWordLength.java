79c171913b1027406e66c06883df5d00d512ef2c
MohammedAli200 <alimohammed3731@gmail.com>
2026-07-15 23:37:17 +0530

    LongestWordLength

---
+++ src/Day72/LongestWordLength.java
@@ -0,0 +1,26 @@
+package Day72;
+
+import java.util.Scanner;
+
+public class LongestWordLength {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String sentence = sc.nextLine();
+
+        String[] words = sentence.split("\\s+");
+
+        String longest = "";
+
+        for (String word : words) {
+
+            if (word.length() > longest.length())
+                longest = word;
+        }
+
+        System.out.println("Longest Word = " + longest);
+        System.out.println("Length = " + longest.length());
+    }
+}
