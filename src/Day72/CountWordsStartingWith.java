30a90332194835db4bccfa6564d3f9170f00087b
MohammedAli200 <alimohammed3731@gmail.com>
2026-07-15 23:37:54 +0530

    CountWordsStartingWith

---
+++ src/Day72/CountWordsStartingWith.java
@@ -0,0 +1,29 @@
+package Day72;
+
+import java.util.Scanner;
+
+public class CountWordsStartingWith {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter sentence: ");
+        String sentence = sc.nextLine().toLowerCase();
+
+        System.out.print("Enter character: ");
+        char ch = Character.toLowerCase(sc.next().charAt(0));
+
+        String[] words = sentence.split("\\s+");
+
+        int count = 0;
+
+        for (String word : words) {
+
+            if (word.charAt(0) == ch)
+                count++;
+        }
+
+        System.out.println("Count = " + count);
+    }
+}
