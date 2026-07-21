238c0efbedd3256feea566916ce6a0cfe3177374
MohammedAli200 <alimohammed3731@gmail.com>
2026-07-21 23:09:51 +0530

    MinimumASCIICharacter

---
+++ src/Day78/MinimumASCIICharacter.java
@@ -0,0 +1,23 @@
+package Day78;
+
+import java.util.Scanner;
+
+public class MinimumASCIICharacter {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        char min = str.charAt(0);
+
+        for (int i = 1; i < str.length(); i++) {
+
+            if (str.charAt(i) < min)
+                min = str.charAt(i);
+        }
+
+        System.out.println("Minimum ASCII Character = " + min);
+    }
+}
