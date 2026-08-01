ae9a13b640d9fc5113206aa94440959d51e0bd2d
MohammedAli200 <alimohammed3731@gmail.com>
2026-08-01 22:15:57 +0530

    OneCharacterDifference

---
+++ src/Day89/OneCharacterDifference.java
@@ -0,0 +1,32 @@
+package Day89;
+
+import java.util.Scanner;
+
+public class OneCharacterDifference {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
+
+        if (s1.length() != s2.length()) {
+            System.out.println("No");
+            return;
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < s1.length(); i++) {
+
+            if (s1.charAt(i) != s2.charAt(i))
+                count++;
+        }
+
+        if (count == 1)
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
+}
