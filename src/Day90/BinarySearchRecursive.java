1bea84e78778eb1e952b7e4b4a8dcadcbe3ded74
MohammedAli200 <alimohammed3731@gmail.com>
2026-08-02 22:41:06 +0530

    BinarySearchRecursive

---
+++ src/Day90/BinarySearchRecursive.java
@@ -0,0 +1,43 @@
+package Day90;
+
+import java.util.Scanner;
+
+public class BinarySearchRecursive {
+
+    static int binarySearch(int[] arr, int low, int high, int key) {
+
+        if (low > high)
+            return -1;
+
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == key)
+            return mid;
+
+        if (arr[mid] > key)
+            return binarySearch(arr, low, mid - 1, key);
+
+        return binarySearch(arr, mid + 1, high, key);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        int key = sc.nextInt();
+
+        int index = binarySearch(arr, 0, n - 1, key);
+
+        if (index != -1)
+            System.out.println("Found at index " + index);
+        else
+            System.out.println("Not Found");
+    }
+}
