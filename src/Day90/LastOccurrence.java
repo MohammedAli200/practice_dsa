cf04b72f2ed2645264a8765da4e8fdf5c1368d62
MohammedAli200 <alimohammed3731@gmail.com>
2026-08-02 22:42:26 +0530

    LastOccurrence

---
+++ src/Day90/LastOccurrence.java
@@ -0,0 +1,39 @@
+package Day90;
+
+import java.util.Scanner;
+
+public class LastOccurrence {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        int key = sc.nextInt();
+
+        int low = 0, high = n - 1;
+        int answer = -1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == key) {
+                answer = mid;
+                low = mid + 1;
+            }
+            else if (arr[mid] < key)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+
+        System.out.println("Last Occurrence = " + answer);
+    }
+}
