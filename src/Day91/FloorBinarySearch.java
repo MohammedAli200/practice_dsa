0c7b0d62de8ff12a9e93664f89ea9c6ce2225347
MohammedAli200 <alimohammed3731@gmail.com>
2026-08-03 22:10:14 +0530

    FloorBinarySearch

---
+++ src/Day91/FloorBinarySearch.java
@@ -0,0 +1,41 @@
+package Day91;
+
+import java.util.Scanner;
+
+public class FloorBinarySearch {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        int key = sc.nextInt();
+
+        int low = 0, high = n - 1;
+        int floor = -1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == key) {
+                floor = arr[mid];
+                break;
+            }
+
+            if (arr[mid] < key) {
+                floor = arr[mid];
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        System.out.println("Floor = " + floor);
+    }
+}
