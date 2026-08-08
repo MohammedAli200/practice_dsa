f52918d9e64bc7a76af31fb5b8254b99ccd15830
MohammedAli200 <alimohammed3731@gmail.com>
2026-08-08 23:12:41 +0530

    Search2DMatrix

---
+++ src/Day96/Search2DMatrix.java
@@ -0,0 +1,49 @@
+package Day96;
+
+import java.util.Scanner;
+
+public class Search2DMatrix {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+
+        int[][] matrix = new int[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        int target = sc.nextInt();
+
+        int low = 0;
+        int high = rows * cols - 1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            int row = mid / cols;
+            int col = mid % cols;
+
+            if (matrix[row][col] == target) {
+                System.out.println(
+                        "Element found at [" + row + "][" + col + "]"
+                );
+                return;
+            }
+
+            if (matrix[row][col] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+
+        System.out.println("Element not found");
+    }
+}
