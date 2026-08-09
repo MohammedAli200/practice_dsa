9568ebce8a45d774250d9e2a8aad37477d2ffaa4
MohammedAli200 <alimohammed3731@gmail.com>
2026-08-09 23:36:46 +0530

    MagneticForce

---
+++ src/Day97/MagneticForce.java
@@ -0,0 +1,62 @@
+package Day97;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MagneticForce {
+
+    static boolean canPlace(int[] position, int balls, int distance) {
+
+        int count = 1;
+        int lastPosition = position[0];
+
+        for (int i = 1; i < position.length; i++) {
+
+            if (position[i] - lastPosition >= distance) {
+
+                count++;
+                lastPosition = position[i];
+
+                if (count >= balls)
+                    return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] position = new int[n];
+
+        for (int i = 0; i < n; i++)
+            position[i] = sc.nextInt();
+
+        int balls = sc.nextInt();
+
+        Arrays.sort(position);
+
+        int low = 1;
+        int high = position[n - 1] - position[0];
+
+        int answer = 0;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (canPlace(position, balls, mid)) {
+                answer = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
