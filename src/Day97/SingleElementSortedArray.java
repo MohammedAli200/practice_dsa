bff10007b7d01256129cf14070b0385244d79152
MohammedAli200 <alimohammed3731@gmail.com>
2026-08-09 23:42:28 +0530

    SingleElementSortedArray

---
+++ src/Day97/SingleElementSortedArray.java
@@ -0,0 +1,37 @@
+package Day97;
+
+import java.util.Scanner;
+
+public class SingleElementSortedArray {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        int low = 0;
+        int high = n - 1;
+
+        while (low < high) {
+
+            int mid = low + (high - low) / 2;
+
+            // Make mid even
+            if (mid % 2 == 1)
+                mid--;
+
+            if (arr[mid] == arr[mid + 1])
+                low = mid + 2;
+            else
+                high = mid;
+        }
+
+        System.out.println("Single Element = " + arr[low]);
+    }
+}
